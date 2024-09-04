43dba852f6393dfc5223f9e014c2ca851d170f16
hanconsol <hasfaraj49@gmail.com>
2024-09-03 19:35:47 -0500

    feat: add return ipaddress

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+  req
+});
+
+// send request ip address
+app.get('/api/whoami', function (req, res) {
+  res.json({ ipaddress: req.ip });
+ 
 });
 
 // listen for requests :)
